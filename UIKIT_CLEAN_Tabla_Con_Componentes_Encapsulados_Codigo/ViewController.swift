f45c4999b06b28a935a716f24f1ebad472f89170
Gonzalo Arques <gonzaloarques@gmail.com>
2025-02-02 13:27:48 +0100

    Initial Commit

---
+++ UIKIT_CLEAN_Tabla_Con_Componentes_Encapsulados_Codigo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKIT_CLEAN_Tabla_Con_Componentes_Encapsulados_Codigo
+//
+//  Created by Gonzalo Arques on 2/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
